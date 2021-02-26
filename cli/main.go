f0976cc6d81e462fdb73e944dafbbafe42595c37
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-25 22:11:47 -0800

    Added codetour file example

--- cli/main.go
+++ cli/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	_, err := os.ReadFile("codetour.yml")
+	if err != nil {
+		fmt.Println("No codetour.yml file found.")
+		return
+	}
+}
