125d35b1602e9cede6ba96ead2f1237cd862fcc2
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-25 23:03:35 -0800

    Removed unused files

--- server/src/App.tsx
+++ server/src/App.tsx
@@ -1,26 +1,10 @@
-import React from "react"
-import logo from "./logo.svg"
-import "./App.css"
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="text-center mt-20">
+        <h1 className="font-bold text-4xl">CodeTour Code Tour</h1>
+        <p className="text-gray-500">Roboto oment</p>
       </header>
     </div>
   )
 }
-
-export default App
